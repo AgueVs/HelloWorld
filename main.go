0b38f6321e15638a1cfb1fcb7463f93ee1521c26
Agueda Vega Soberon (eaguveg) <agueda.vega.soberon@ericsson.com>
2024-11-11 12:41:56 +0100

    incluir comentarios a main

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"github.com/aguevs/HelloWorld/funciones"
 )
-
+// Funcion que devuelve Hello World
 func helloworld() string {
 	return "Hello World!!"
 }
 
+// Funcion Main;
+//  Ejercicio1: nos imprime por consola "Hola Mundo" en Ingles y Chino.
+//                Hola Mundo en ingles usando una funcion creada en este paquete main.
+//                Hola Mundo en chino usando una funcion creada en otro paquete llamado funciones creado por mi.
+//  Ejercicio2:
 func main() {
 	fmt.Println(helloworld())
 	fmt.Println(funciones.Chino())
